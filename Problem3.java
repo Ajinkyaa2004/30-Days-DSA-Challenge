25770e96b2078f105d60d642fdb229688d5c09fb
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-08-20 13:46:03 +0000

    Find the missing number in 1…n

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,21 @@
 // Find the missing number in 1…n
+import java.util.Scanner;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n (range 1 to n): ");
+        int n = sc.nextInt();
+        int[] arr = new int[n - 1]; 
+        System.out.println("Enter " + (n - 1) + " numbers:");
+        int sum = 0;
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        int totalSum = n * (n + 1) / 2;
+        int missing = totalSum - sum;
+        System.out.println("Missing number is: " + missing);
+        sc.close();
+    }
+}
