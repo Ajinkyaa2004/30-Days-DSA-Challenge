2549c4cf10ad59d36f08c4025859c8569d40056e
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-08-22 14:27:30 +0000

    Find the second largest element

--- Problem5.java
+++ Problem5.java
@@ -1,1 +1,42 @@
 // Find the second largest element
+import java.util.Scanner;
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        if (n < 2) {
+            System.out.println("Array must have at least two elements!");
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > first) {
+                second = first;
+                first = arr[i];
+            } else if (arr[i] > second && arr[i] != first) {
+                second = arr[i];
+            }
+        }
+
+        if (second == Integer.MIN_VALUE) {
+            System.out.println("No second largest element (all elements are same).");
+        } else {
+            System.out.println("Second largest element is: " + second);
+        }
+
+        sc.close();
+    }
+}
