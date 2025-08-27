d63b9c9828f9e25391481c47ba4c2ea34829aad1
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-08-27 04:47:28 +0000

    Find the first non-repeating character

--- Problem10.java
+++ Problem10.java
@@ -1,1 +1,29 @@
 // Find the first non-repeating character
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FirstNonRepeating {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
+        }
+        char result = '\0';
+        for (char ch : str.toCharArray()) {
+            if (freqMap.get(ch) == 1) {
+                result = ch;
+                break;
+            }
+        }
+        if (result != '\0') {
+            System.out.println("First non-repeating character: " + result);
+        } else {
+            System.out.println("No non-repeating character found.");
+        }
+        sc.close();
+    }
+}
