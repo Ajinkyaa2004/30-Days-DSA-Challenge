6be3563411020ba5ee868dd4de1c967dfcfe10b1
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-08-31 05:42:35 +0000

    Frequency of elements in an array

--- Problem13.java
+++ Problem13.java
@@ -1,1 +1,19 @@
 // Frequency of elements in an array
+import java.util.*;
+
+public class FrequencyOfElements {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 20, 10, 10, 30, 40, 20};
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : arr) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println("Element frequencies:");
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            System.out.println(entry.getKey() + " → " + entry.getValue());
+        }
+    }
+}
