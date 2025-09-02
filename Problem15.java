9816458a522a84c589f60d746bfdd0c11e09738f
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-09-02 05:06:50 +0000

    Subarray with given sum

--- Problem15.java
+++ Problem15.java
@@ -1,1 +1,27 @@
 // Subarray with given sum
+
+public class SubarraySum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 7, 5};
+        int target = 12;
+
+        int start = 0, sum = 0;
+
+        for (int end = 0; end < arr.length; end++) {
+            sum += arr[end];
+
+           
+            while (sum > target && start < end) {
+                sum -= arr[start];
+                start++;
+            }
+
+            if (sum == target) {
+                System.out.println("Subarray found from index " + start + " to " + end);
+                return;
+            }
+        }
+
+        System.out.println("No subarray with given sum found");
+    }
+}
