5e2be497206ffe51737ba7f78b0f4211c8056724
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-09-03 05:50:57 +0000

    Factorial of a number

--- Problem16.java
+++ Problem16.java
@@ -1,1 +1,15 @@
 // Factorial of a number
+
+public class FactorialRec {
+    static long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Factorial of " + n + " is: " + factorial(n));
+    }
+}
