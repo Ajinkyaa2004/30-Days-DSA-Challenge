ab869154e46383984f06bef151ececc2d145dd66
Ajinkya Dhumal <dhumalajinkya2004@gmail.com>
2025-09-04 14:39:37 +0000

    Fibonacci sequence (recursive)

--- Problem17.java
+++ Problem17.java
@@ -1,1 +1,19 @@
 // Fibonacci sequence (recursive)
+
+public class FibonacciRecursive {
+    
+    static int fibonacci(int n) {
+        if (n == 0) return 0;   // base case
+        if (n == 1) return 1;   // base case
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int terms = 10; 
+
+        System.out.print("Fibonacci Sequence: ");
+        for (int i = 0; i < terms; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
